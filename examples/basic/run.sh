6b28ce89a46a3dd2c31118b7ab927e35ac3b4e7b
SabrinaJewson <sejewson@gmail.com>
2023-10-31 21:14:56 +0000

    Improve example run.sh

--- examples/basic/run.sh
+++ examples/basic/run.sh
@@ -7,12 +7,8 @@ cd "$(dirname "$0")"
 cargo build
 cd ../..
 
-mkdir -p "$ROFI_PREFIX"/lib/rofi
-if ! cp target/debug/librofi_plugin_example_basic.so "$ROFI_PREFIX"/lib/rofi/plugin_example_basic.so
-then
-	echo Attempting to copy again as root
-	sudo cp target/debug/librofi_plugin_example_basic.so "$ROFI_PREFIX"/lib/rofi/plugin_example_basic.so
-fi
-
-#gdb --args \
-	"$ROFI_PREFIX"/bin/rofi -modi run,plugin-example-basic -show plugin-example-basic "$@"
+#DEBUGGER can be e.g. "gdb --args"
+ROFI_PLUGIN_PATH=target/debug ${DEBUGGER:-} "$ROFI_PREFIX"/bin/rofi \
+	-modi run,plugin-example-basic \
+	-show plugin-example-basic \
+	"$@"
